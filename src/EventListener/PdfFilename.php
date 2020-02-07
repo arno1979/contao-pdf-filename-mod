0b0413318aa4e7f6c68551a7a337b9c79f2692bd
Arno Schumacher <arno_schumacher@gmx.de>
2020-02-07 17:54:10 +0100

    WIP

--- src/EventListener/PdfFilename.php
+++ src/EventListener/PdfFilename.php
@@ -6,12 +6,12 @@ use Contao\CoreBundle\ServiceAnnotation\Hook;
 use Contao\ModuleArticle;
 use Terminal42\ServiceAnnotationBundle\ServiceAnnotationInterface;
 
-class PageHook implements ServiceAnnotationInterface
+class PdfFilename implements ServiceAnnotationInterface
 {
     /**
      * @Hook("printArticleAsPdf", priority=10)
      */
-    public function onGeneratePage(string $articleContent, ModuleArticle $module): void
+    public function onprintArticleAsPdf(string $articleContent, ModuleArticle $module): void
     {
         \log_message('Test2','arno.log');
 //          $module->title = $module->alias;
